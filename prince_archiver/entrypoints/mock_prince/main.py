afe80c419688f0af31a6afab1486e2ab83b5e573
coco <corentin@bisot.com>
2024-12-16 10:50:21 +0100

    changed redis dsn

--- prince_archiver/entrypoints/mock_prince/main.py
+++ prince_archiver/entrypoints/mock_prince/main.py
@@ -23,6 +23,8 @@ from typing import Optional
 
 LOGGER = logging.getLogger(__name__)
 
+REDIS_DSN = "redis://tsu-dsk001.ipa.amolf.nl:6380"
+
 
 
 def _create_event(row) -> NewImagingEvent:
@@ -73,7 +75,6 @@ def _create_event(row) -> NewImagingEvent:
         local_path=row["total_path"],
     )
 
-REDIS_DSN = "redis://tsu-dsk001.ipa.amolf.nl:6380"
 def create_app() -> FastAPI:
     client = redis.from_url(REDIS_DSN)
 
