b5d541a000e42980db60080385e22ad01163a1c6
coco <corentin@bisot.com>
2025-06-18 17:10:39 +0200

    1st potentially working version

--- prince_archiver/entrypoints/mock_prince/util.py
+++ prince_archiver/entrypoints/mock_prince/util.py
@@ -213,7 +213,7 @@ def load_processed_rows():
     if os.path.exists(PROCESSED_ROWS_FILE):
         return pd.read_csv(PROCESSED_ROWS_FILE)
     print("creating file")
-    return pd.DataFrame(columns=["folder","old_id","Morrison_id"])
+    return pd.DataFrame(columns=["folder","old_id","Morrison_id","DateTime"])
 
 def save_processed_rows(processed_rows):
     processed_rows.to_csv(PROCESSED_ROWS_FILE, index=False)
